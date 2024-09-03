06ec04420f0936c0efd93f9b05f9681d33a570a4
Schn777 <Schneideremile547@gmail.com>
2024-09-03 14:51:56 -0400

    laboratoire1 v1.2

--- Prototype01/Assets/Scripts/PlayerController.cs
+++ Prototype01/Assets/Scripts/PlayerController.cs
@@ -9,10 +9,11 @@ public class PlayerController : MonoBehaviour
     private float turnSpeed = 100f;
     private float horizontalInput;
     private float forwardInput;
+    private bool isStart;
     // Start is called before the first frame update
     void Start()
     {
-        transform.Translate(Vector3.forward * speed * Time.deltaTime);
+        isStart = true;
     }
 
     // Update is called once per frame
@@ -21,8 +22,15 @@ public class PlayerController : MonoBehaviour
         horizontalInput = Input.GetAxis("Horizontal");
         forwardInput = Input.GetAxis("Vertical");
         //Move the vehicule foward
-
         transform.Translate(Vector3.forward * speed * Time.deltaTime * forwardInput);
         transform.Rotate(Vector3.up * turnSpeed * Time.deltaTime * horizontalInput);
+        if (isStart)
+        {
+            transform.Translate(Vector3.forward * speed * Time.deltaTime);
+        }
+        if (Input.GetKeyDown(KeyCode.DownArrow) || Input.GetKeyDown(KeyCode.UpArrow))
+        {
+            isStart = !isStart;
+        }
     }
 }
