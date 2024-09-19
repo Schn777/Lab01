bfb2cf07cb73a329db6a7de0589c652100b565bc
Schn777 <Schneideremile547@gmail.com>
2024-09-19 13:58:40 -0400

    lab03 v0.1

---
+++ Assets/Scripts/AttackPlayer.cs
@@ -0,0 +1,42 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class AttackPlayer : MonoBehaviour
+{
+    //https://discussions.unity.com/t/detect-if-player-is-in-range/99984
+    public GameObject player;
+    Animator creatureAnimator;
+    public int maxRange;
+    public int minRange;
+    public List<AudioClip> listAudioClip;
+
+    public AudioSource audioSource;
+    private bool isOnTimePlay;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        creatureAnimator = GetComponent<Animator>();
+        player = GameObject.FindWithTag("Player");
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+            
+        if ((Vector3.Distance(transform.position, player.transform.position) < maxRange)
+           && (Vector3.Distance(transform.position, player.transform.position) > minRange))
+        {
+            creatureAnimator.SetBool("Attack", true);
+            if (!isOnTimePlay) {
+                audioSource.PlayOneShot(listAudioClip[Random.Range(0, listAudioClip.Count)]);
+                isOnTimePlay = true;
+            }
+
+        } else {
+            creatureAnimator.SetBool("Attack", false);
+            isOnTimePlay = false;
+        }
+    }
+}
